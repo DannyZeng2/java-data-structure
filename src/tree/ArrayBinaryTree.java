a2f7da1a09f5ea67dd4f67efe7f4828783062079
曾志威 <>
2021-08-11 21:52:17 +0800

    Array binary tree

---
+++ src/tree/ArrayBinaryTree.java
@@ -0,0 +1,25 @@
+package tree;
+
+import java.util.Collections;
+
+public class ArrayBinaryTree {
+    int[] data;
+
+    public ArrayBinaryTree(int[] data) {
+        this.data = data;
+    }
+
+    public void vlrPrint(int index) {
+        if(data == null || data.length ==0) {
+            return;
+        }
+        System.out.println(data[index]);
+        if(2*index+1<data.length) {
+            vlrPrint(2*index+1);
+        }
+        if(2*index+2<data.length) {
+            vlrPrint(2*index+2);
+        }
+    }
+
+}
---
+++ test/tree/ArrayBinaryTreeTest.java
@@ -0,0 +1,14 @@
+package tree;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArrayBinaryTreeTest {
+
+    @Test
+    public void vlrPrint() {
+        ArrayBinaryTree tree = new ArrayBinaryTree(new int[]{1,2,3,4,5,6,7});
+        tree.vlrPrint(10);
+    }
+}
